d6d622f2f9acd64b3c90605be33fdfca2a359271
cassiopga <cassiopga@gmail.com>
2021-08-19 17:33:32 -0300

    Criação Departamento

---
+++ App_Piemonte/Controllers/DepartamentoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App_Piemonte.Models;
+namespace App_Piemonte.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Departamento> list = new List<Departamento>
+            {
+                new Departamento { id = 1, Name = "Vendas" },
+                new Departamento { id = 2, Name = "Operacional" },
+                new Departamento { id = 3, Name = "Financeiro" },
+                new Departamento { id = 4, Name = "Contratos" },
+                new Departamento { id = 5, Name = "Especialista" }
+            };
+            return View(list);
+        }
+    }
+}
--- App_Piemonte/Controllers/HomeController.cs
+++ App_Piemonte/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using App_Piemonte.Controllers;
 
 namespace App_Piemonte.Controllers
 {
@@ -38,7 +39,7 @@ namespace App_Piemonte.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new Models.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
