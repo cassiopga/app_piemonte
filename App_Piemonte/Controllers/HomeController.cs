21f8fc27e4e921547716d2eb92fc362488349662
cassiopga <cassiopga@gmail.com>
2021-08-19 12:51:42 -0300

    Incio

---
+++ App_Piemonte/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using App_Piemonte.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App_Piemonte.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            string bv = "Seja bem vindo  a nossa aplicação";
+            ViewData["Message"] = "App Piemonte Promotora";
+            ViewData["Boa Vinda"] = bv;
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
